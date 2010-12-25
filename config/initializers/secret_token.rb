d4f81f4a878ff3a6b3d15195a7ec08c48013a0db
Colby Blaisdell <ccblaisdell@gmail.com>
2010-12-24 22:19:43 -0600

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gotissues::Application.config.secret_token = '39dc8d559d3f29ca211b4e580c58979bede13574a4523788d0e60f3339f82e7010e7edcd9899db53925f54755183ea0a56a3f1c24c57b0a5e1c25847ecef7088'
