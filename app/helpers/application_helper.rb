14d3be97b5805e29cc537163418054856299ce07
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2011-01-03 15:55:01 -0600

    Added framework to customize title elements. Some elements have been customized

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def markdown(text)
     RDiscount.new(text).to_html.html_safe
   end
+  
+  def page_title(title = nil)
+    if title
+      @title = title
+      return true
+    else
+      return @title || "Got Issues"
+    end
+  end
 end
