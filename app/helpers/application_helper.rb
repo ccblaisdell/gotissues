11739270da69c6dde98946db3ad53bc34b7e4e28
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2011-01-07 15:56:57 -0600

    Added some gratuitous styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,24 @@ module ApplicationHelper
       return @title || "Got Issues"
     end
   end
+  
+  
+  # breadrumbs
+  def breadcrumbs_for(*args)
+    options, args = args[-1], args[0..-2] if args[-1].kind_of?(Hash)
+    options ||= {}
+    @breadcrumbs = raw("<div class='breadcrumbs #{options[:class] }'>
+      #{args.join( ' / ')}
+    </div>") rescue ""
+  end
+  
+  def home_crumb
+    link_to "Home", root_path
+  end
+  def projects_crumb
+    link_to "Projects", projects_path
+  end
+  def project_crumb(project)
+    link_to project.name, project_issues_path(project)
+  end
 end
